9555db497ea3a8c15bef5503ee38d90d8364d55d
Connor <cmpopp1@yahoo.com>
2019-10-04 14:41:03 -0500

    Created basic camera movement example

--- WindyEngine/Source.cpp
+++ WindyEngine/Source.cpp
@@ -13,56 +13,13 @@ void Windy::Begin() {
 	anvil.transform.rotation = Vector3<double>(0, 0, 0);
 	AddGameObject(anvil);
 
-	/*GameObject target;
-	target.mesh = Mesh("C:\\Users\\Colloquiem\\Desktop\\Shape.obj");
-	target.transform.position = Vector3<double>(-5, 0, 10);
-	target.transform.scale = Vector3<double>(.1, .1, .1);
-	AddGameObject(target);*/
-
-	/*
-	GameObject cube2, cube3, cube4, cube5;;
-
-	cube2.transform.position = Vector3<double>(4, 2, 10);
-	cube3.transform.position = Vector3<double>(4, -2, 10);
-	cube4.transform.position = Vector3<double>(-4, 2, 10);
-	cube5.transform.position = Vector3<double>(-4, -2, 10);
-	
-	AddGameObject(cube2);
-	AddGameObject(cube3);
-	AddGameObject(cube4);
-	AddGameObject(cube5);
-	*/
-
-	/*
-	GameObject cube;
-	for (int x = -3; x < 3; x++) {
-		for (int y = -3; y < 3; y++) {
-			for (int z = -3; z < 3; z++) {
-				cube.transform.position = Vector3<double>(x * 2, y * 2, (z * 2) + 50 );
-				AddGameObject(cube);
-			}
-		}
-	}
-	*/
 }
 	
 //Called once per frame
 void Windy::Update() {
 	static double angle[] = { 0,180,0 }, angleInc = 1;
-
-	/*if (gameObjects[0].transform.position.X - gameObjects[1].transform.position.X < 1
-		&& gameObjects[0].transform.position.Y - gameObjects[1].transform.position.Y < 1
-		&& gameObjects[0].transform.position.Z - gameObjects[1].transform.position.Z < 1) 
-	{
-		gameObjects[1].transform.position = Vector3<double>((rand() % 5) - 2.5, (rand() % 20) - 5, (rand() % 10) + 10);
-	}*/
-
-	/*
-	for (int i = 1; i < gameObjects.size(); i++) {
-		gameObjects[i].transform.position.X *= 1.01;
-		gameObjects[i].transform.position.Y *= 1.01;
-	}
-	*/
+	static Vector3<double> camPos, camRot, camScale = Vector3<double>(1,1,1);
+	double tSpeed = 0.5, rSpeed = 5;
 
 	for (int axis = 0; axis < 3; axis++) {
 		if (angle[axis] > 360) {
@@ -74,27 +31,61 @@ void Windy::Update() {
 	}
 	if (WInput.KeyHeld(KC_W) || WInput.KeyDown(KC_W)) {
 		angle[2] += angleInc * deltaTime * 10;
-		gameObjects[0].transform.position.Y += angleInc * deltaTime;
+		camPos.Z += tSpeed * deltaTime;
+		//gameObjects[0].transform.position.Y += angleInc * deltaTime;
 	}
 	if (WInput.KeyHeld(KC_S) || WInput.KeyDown(KC_S)) {
 		angle[2] -= angleInc * deltaTime * 10;
-		gameObjects[0].transform.position.Y -= angleInc * deltaTime;
+		camPos.Z -= tSpeed * deltaTime;
+		//gameObjects[0].transform.position.Y -= angleInc * deltaTime;
 	}
 	if (WInput.KeyHeld(KC_D) || WInput.KeyDown(KC_D)) {
 		angle[1] += angleInc * deltaTime * 10;
+		camPos.X += tSpeed * deltaTime;
 		//gameObjects[0].transform.position.X += angleInc * deltaTime;
 	}
 	if (WInput.KeyHeld(KC_A) || WInput.KeyDown(KC_A)) {
 		angle[1] -= angleInc * deltaTime * 10;
+		camPos.X -= tSpeed * deltaTime;
 		//gameObjects[0].transform.position.X -= angleInc * deltaTime;
 	}
 	if (WInput.KeyHeld(KC_E) || WInput.KeyDown(KC_E)) {
 		angle[0] -= angleInc * deltaTime * 10;
+		camPos.Y -= tSpeed * deltaTime;
 		//gameObjects[0].transform.position.Z += angleInc * deltaTime;
 	}
 	if (WInput.KeyHeld(KC_Q) || WInput.KeyDown(KC_Q)) {
 		angle[0] += angleInc * deltaTime * 10;
+		camPos.Y += tSpeed * deltaTime;
 		//gameObjects[0].transform.position.Z -= angleInc  * deltaTime;
+	}
+
+	//----------------------------------------------------------------------------------------------
+	
+	if (WInput.KeyHeld(KC_I) || WInput.KeyDown(KC_I)) {
+		camRot.X += rSpeed * deltaTime;
+	}
+	if (WInput.KeyHeld(KC_K) || WInput.KeyDown(KC_K)) {
+		camRot.X -= rSpeed * deltaTime;
+	}
+	if (WInput.KeyHeld(KC_L) || WInput.KeyDown(KC_L)) {
+		camRot.Y += rSpeed * deltaTime;
+	}
+	if (WInput.KeyHeld(KC_J) || WInput.KeyDown(KC_J)) {
+		camRot.Y -= rSpeed * deltaTime;
+	}
+	if (WInput.KeyHeld(KC_O) || WInput.KeyDown(KC_O)) {
+		camRot.Z -= rSpeed * deltaTime;
+	}
+	if (WInput.KeyHeld(KC_U) || WInput.KeyDown(KC_U)) {
+		camRot.Z += rSpeed * deltaTime;
+	}
+
+	if (WInput.KeyHeld(KC_T) || WInput.KeyDown(KC_T)) {
+		camScale.Translate(tSpeed * deltaTime, tSpeed * deltaTime, tSpeed * deltaTime);
+	}
+	if (WInput.KeyHeld(KC_G) || WInput.KeyDown(KC_G)) {
+		camScale.Translate(-tSpeed * deltaTime, -tSpeed * deltaTime, -tSpeed * deltaTime);
 	}
 	
 	if (WInput.KeyUp(KC_R)) {
@@ -108,5 +99,8 @@ void Windy::Update() {
 	}
 	
 	gameObjects[0].transform.rotation = Vector3<double>(angle[0], angle[1], angle[2]);
+	GetMainWindow().GetCamera().SetPosition(camPos);
+	GetMainWindow().GetCamera().SetRotation(camRot);
+	GetMainWindow().GetCamera().SetScale(camScale);
 }
 
