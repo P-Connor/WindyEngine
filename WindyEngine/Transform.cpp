9a98565c008f9c2384e20a3c0bcdb99a789cb3ee
Connor <cmpopp1@yahoo.com>
2021-02-03 19:20:05 -0600

    Added Translate, TranslateWorld, Rotate, and Scale

--- WindyEngine/Transform.cpp
+++ WindyEngine/Transform.cpp
@@ -7,3 +7,51 @@ Transform::Transform() {
 	scale = Vector3<double>(1, 1, 1);
 
 }
+
+void Transform::Translate(double x, double y, double z) {
+	Translate(Vector3<double>(x, y, z));
+}
+void Transform::Translate(Vector3<double> vec) {
+	position += Matrix4::RotationDeg(rotation) * vec;
+}
+
+void Transform::TranslateWorld(double x, double y, double z) {
+	position.X += x;
+	position.Y += y;
+	position.Z += z;
+}
+void Transform::TranslateWorld(Vector3<double> vec) {
+	position += vec;
+}
+
+void Transform::Rotate(double x, double y, double z) {
+	rotation.X += x;
+	if (rotation.X > 360)
+		rotation.X -= (int)(rotation.X / 360) * 360;
+	if (rotation.X < 0)
+		rotation.X += ((int)(-rotation.X / 360) + 1) * 360;
+
+	rotation.Y += y;
+	if (rotation.Y > 360)
+		rotation.Y -= (int)(rotation.Y / 360) * 360;
+	if (rotation.Y < 0)
+		rotation.Y += ((int)(-rotation.Y / 360) + 1) * 360;
+
+	rotation.Z += z;
+	if (rotation.Z > 360)
+		rotation.Z -= (int)(rotation.Z / 360) * 360;
+	if (rotation.Z < 0)
+		rotation.Z += ((int)(-rotation.Z / 360) + 1) * 360;
+}
+void Transform::Rotate(Vector3<double> vec) {
+	Rotate(vec.X, vec.Y, vec.Z);
+}
+
+void Transform::Scale(double x, double y, double z) {
+	scale.X += x;
+	scale.Y += y;
+	scale.Z += z;
+}
+void Transform::Scale(Vector3<double> vec) {
+	scale += vec;
+}
--- WindyEngine/Transform.h
+++ WindyEngine/Transform.h
@@ -1,11 +1,24 @@
 #pragma once
 #include "Component.h"
 #include "Vector3.h"
+#include "Matrix4.h"
 
 class Transform : Component {
 public:
 	Transform();
 
 	Vector3<double> position, rotation, scale;
+
+	void Translate(double x, double y, double z);
+	void Translate(Vector3<double> vec);
+
+	void TranslateWorld(double x, double y, double z);
+	void TranslateWorld(Vector3<double> vec);
+
+	void Rotate (double x, double y, double z);
+	void Rotate (Vector3<double> vec);
+
+	void Scale (double x, double y, double z);
+	void Scale (Vector3<double> vec);
 
 };
