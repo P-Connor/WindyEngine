8bf25d4bb4e0848d7a1ecb4ccbde839f90f3579d
Connor <cmpopp1@yahoo.com>
2021-02-03 19:25:44 -0600

    Updated simple camera movement example

--- WindyEngine/Source.cpp
+++ WindyEngine/Source.cpp
@@ -13,12 +13,14 @@ void Windy::Begin() {
 	anvil.transform.rotation = Vector3<double>(0, 0, 0);
 	AddGameObject(anvil);
 
+	GetMainWindow().GetCamera().GetTransform().Rotate(0, 180, 0);
 }
 	
 //Called once per frame
 void Windy::Update() {
 	static double angle[] = { 0,180,0 }, angleInc = 1;
-	static Vector3<double> camPos, camRot, camScale = Vector3<double>(1,1,1);
+	Vector3<double> camVelocity = Vector3<double>(0, 0, 0);
+	Vector3<double> camRotationVelocity = Vector3<double>(0, 0, 0);
 	double tSpeed = 0.5, rSpeed = 5;
 
 	for (int axis = 0; axis < 3; axis++) {
@@ -30,62 +32,55 @@ void Windy::Update() {
 		}
 	}
 	if (WInput.KeyHeld(KC_W) || WInput.KeyDown(KC_W)) {
-		angle[2] += angleInc * deltaTime * 10;
-		camPos.Z += tSpeed * deltaTime;
+		//angle[2] += angleInc * deltaTime * 10;
+		camVelocity.Z = tSpeed * deltaTime;
 		//gameObjects[0].transform.position.Y += angleInc * deltaTime;
 	}
 	if (WInput.KeyHeld(KC_S) || WInput.KeyDown(KC_S)) {
-		angle[2] -= angleInc * deltaTime * 10;
-		camPos.Z -= tSpeed * deltaTime;
+		//angle[2] -= angleInc * deltaTime * 10;
+		camVelocity.Z = -tSpeed * deltaTime;
 		//gameObjects[0].transform.position.Y -= angleInc * deltaTime;
 	}
 	if (WInput.KeyHeld(KC_D) || WInput.KeyDown(KC_D)) {
-		angle[1] += angleInc * deltaTime * 10;
-		camPos.X += tSpeed * deltaTime;
+		//angle[1] += angleInc * deltaTime * 10;
+		camVelocity.X = tSpeed * deltaTime;
 		//gameObjects[0].transform.position.X += angleInc * deltaTime;
 	}
 	if (WInput.KeyHeld(KC_A) || WInput.KeyDown(KC_A)) {
-		angle[1] -= angleInc * deltaTime * 10;
-		camPos.X -= tSpeed * deltaTime;
+		//angle[1] -= angleInc * deltaTime * 10;
+		camVelocity.X = -tSpeed * deltaTime;
 		//gameObjects[0].transform.position.X -= angleInc * deltaTime;
 	}
 	if (WInput.KeyHeld(KC_E) || WInput.KeyDown(KC_E)) {
-		angle[0] -= angleInc * deltaTime * 10;
-		camPos.Y -= tSpeed * deltaTime;
+		//angle[0] -= angleInc * deltaTime * 10;
+		camVelocity.Y = -tSpeed * deltaTime;
 		//gameObjects[0].transform.position.Z += angleInc * deltaTime;
 	}
 	if (WInput.KeyHeld(KC_Q) || WInput.KeyDown(KC_Q)) {
-		angle[0] += angleInc * deltaTime * 10;
-		camPos.Y += tSpeed * deltaTime;
+		//angle[0] += angleInc * deltaTime * 10;
+		camVelocity.Y = tSpeed * deltaTime;
 		//gameObjects[0].transform.position.Z -= angleInc  * deltaTime;
 	}
 
 	//----------------------------------------------------------------------------------------------
 	
 	if (WInput.KeyHeld(KC_I) || WInput.KeyDown(KC_I)) {
-		camRot.X += rSpeed * deltaTime;
+		camRotationVelocity.X = rSpeed * deltaTime;
 	}
 	if (WInput.KeyHeld(KC_K) || WInput.KeyDown(KC_K)) {
-		camRot.X -= rSpeed * deltaTime;
+		camRotationVelocity.X = -rSpeed * deltaTime;
 	}
 	if (WInput.KeyHeld(KC_L) || WInput.KeyDown(KC_L)) {
-		camRot.Y += rSpeed * deltaTime;
+		camRotationVelocity.Y = rSpeed * deltaTime;
 	}
 	if (WInput.KeyHeld(KC_J) || WInput.KeyDown(KC_J)) {
-		camRot.Y -= rSpeed * deltaTime;
+		camRotationVelocity.Y = -rSpeed * deltaTime;
 	}
 	if (WInput.KeyHeld(KC_O) || WInput.KeyDown(KC_O)) {
-		camRot.Z -= rSpeed * deltaTime;
+		camRotationVelocity.Z = -rSpeed * deltaTime;
 	}
 	if (WInput.KeyHeld(KC_U) || WInput.KeyDown(KC_U)) {
-		camRot.Z += rSpeed * deltaTime;
-	}
-
-	if (WInput.KeyHeld(KC_T) || WInput.KeyDown(KC_T)) {
-		camScale.Translate(tSpeed * deltaTime, tSpeed * deltaTime, tSpeed * deltaTime);
-	}
-	if (WInput.KeyHeld(KC_G) || WInput.KeyDown(KC_G)) {
-		camScale.Translate(-tSpeed * deltaTime, -tSpeed * deltaTime, -tSpeed * deltaTime);
+		camRotationVelocity.Z = rSpeed * deltaTime;
 	}
 	
 	if (WInput.KeyUp(KC_R)) {
@@ -99,8 +94,11 @@ void Windy::Update() {
 	}
 	
 	gameObjects[0].transform.rotation = Vector3<double>(angle[0], angle[1], angle[2]);
-	GetMainWindow().GetCamera().SetPosition(camPos);
-	GetMainWindow().GetCamera().SetRotation(camRot);
-	GetMainWindow().GetCamera().SetScale(camScale);
+	Camera& c = GetMainWindow().GetCamera();
+	Transform& t = c.GetTransform();
+	WinDebug.Log(std::to_string(t.rotation.X) + " " + std::to_string(t.rotation.Y) + " " + std::to_string(t.rotation.Z));
+	t.Translate(camVelocity);
+	t.Rotate(camRotationVelocity);
+	c.UpdViewMatrix();
 }
 
