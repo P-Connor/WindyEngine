843b9819920a6c4ec2f7bcda1b9680d871a06843
Connor <cmpopp1@yahoo.com>
2021-02-03 19:30:22 -0600

    Added support for +, -, *, and / operators

--- WindyEngine/Vector3.h
+++ WindyEngine/Vector3.h
@@ -11,9 +11,18 @@ public:
 
 	Vector3<T>& operator= (const Vector3<T>& vec);
 
-	Vector3<T>& operator* (const T&) const;
-	Vector3<T>& operator* (const Vector3<T>&) const;
+	Vector3<T> operator+ (const T&) const;
+	Vector3<T> operator+ (const Vector3<T>&) const;
+	Vector3<T>& operator+= (const Vector3<T>&);
+	Vector3<T> operator- (const T&) const;
+	Vector3<T> operator- (const Vector3<T>&) const;
+	Vector3<T>& operator-= (const Vector3<T>&);
+	Vector3<T> operator* (const T&) const;
+	Vector3<T> operator* (const Vector3<T>&) const;
 	Vector3<T>& operator*= (const Vector3<T>&);
+	Vector3<T> operator/ (const T&) const;
+	Vector3<T> operator/ (const Vector3<T>&) const;
+	Vector3<T>& operator/= (const Vector3<T>&);
 
 	T X, Y, Z;
 
@@ -46,7 +55,7 @@ Vector3<T>::Vector3(const T& x, const T& y, const T& z)
 }
 
 template<class T>
-Vector3<T>& Vector3<T>::operator= (const Vector3<T>& vec) {
+inline Vector3<T>& Vector3<T>::operator= (const Vector3<T>& vec) {
 	X = vec.X;
 	Y = vec.Y;
 	Z = vec.Z;
@@ -54,30 +63,83 @@ Vector3<T>& Vector3<T>::operator= (const Vector3<T>& vec) {
 }
 
 template<class T>
-Vector3<T>& Vector3<T>::operator* (const T& scalar) const
+inline Vector3<T> Vector3<T>::operator+(const T& scalar) const
 {
-	Vector3<T> ret;
-	ret.X *= scalar;
-	ret.Y *= scalar;
-	ret.Z *= scalar;
-	return ret;
+	return Vector3<T>(X + scalar, Y + scalar, Z + scalar);
 }
 
 template<class T>
-Vector3<T>& Vector3<T>::operator*(const Vector3<T>& vec) const
-{
-	Vector3<T> ret;
-	ret.X *= vec.X;
-	ret.Y *= vec.Y;
-	ret.Z *= vec.Z;
-	return ret;
+inline Vector3<T> Vector3<T>::operator+ (const Vector3<T>& vec) const {
+	return Vector3<T>(X + vec.X, Y + vec.Y, Z + vec.Z);
 }
 
 template<class T>
-Vector3<T>& Vector3<T>::operator*= (const Vector3<T>& vec) {
+inline Vector3<T>& Vector3<T>::operator+=(const Vector3<T>& vec)
+{
+	X += vec.X;
+	Y += vec.Y;
+	Z += vec.Z;
+	return *this;
+}
+
+template<class T>
+inline Vector3<T> Vector3<T>::operator-(const T& scalar) const
+{
+	return Vector3<T>(X - scalar, Y - scalar, Z - scalar);
+}
+
+template<class T>
+inline Vector3<T> Vector3<T>::operator- (const Vector3<T>& vec) const {
+	return Vector3<T>(X - vec.X, Y - vec.Y, Z - vec.Z);
+}
+
+template<class T>
+inline Vector3<T>& Vector3<T>::operator-=(const Vector3<T>& vec)
+{
+	X -= vec.X;
+	Y -= vec.Y;
+	Z -= vec.Z;
+	return *this;
+}
+
+
+template<class T>
+inline Vector3<T> Vector3<T>::operator*(const T& scalar) const
+{
+	return Vector3<T>(X * scalar, Y * scalar, Z * scalar);
+}
+
+template<class T>
+inline Vector3<T> Vector3<T>::operator* (const Vector3<T>& vec) const {
+	return Vector3<T>(X * vec.X, Y * vec.Y, Z * vec.Z);
+}
+
+template<class T>
+inline Vector3<T>& Vector3<T>::operator*=(const Vector3<T>& vec)
+{
 	X *= vec.X;
 	Y *= vec.Y;
 	Z *= vec.Z;
+	return *this;
+}
+
+template<class T>
+inline Vector3<T> Vector3<T>::operator/(const T& scalar) const
+{
+	return Vector3<T>(X / scalar, Y / scalar, Z / scalar);
+}
+
+template<class T>
+inline Vector3<T> Vector3<T>::operator/ (const Vector3<T>& vec) const {
+	return Vector3<T>(X / vec.X, Y / vec.Y, Z / vec.Z);
+}
+
+template<class T>
+inline Vector3<T>& Vector3<T>::operator/=(const Vector3<T>& vec)
+{
+	X /= vec.X;
+	Y /= vec.Y;
+	Z /= vec.Z;
 	return *this;
 }
 
